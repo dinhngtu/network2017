0ef9208ebf5928bcbd4f0a482170931e9980def4
Dinh Ngoc Tu <none@none>
2017-03-22 18:46:57 +0700

    add practical work 2: gethostbyname()

---
+++ 02.practical.work.gethostbyname.c
@@ -0,0 +1,34 @@
+#define _BSD_SOURCE
+#define _XOPEN_SOURCE 700
+
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char **argv) {
+    size_t bufsize;
+    char *name = NULL;
+
+    if (argc == 1) {
+        printf("Enter hostname: ");
+        int nchar = getline(&name, &bufsize, stdin);
+        if (nchar < 1) {
+            fprintf(stderr, "Bad name\n");
+            return 1;
+        }
+        name[nchar - 2] = 0; // erase the \n
+    } else {
+        name = argv[1];
+    }
+
+    struct hostent *he = gethostbyname(name);
+    for (char **addr = he->h_addr_list; *addr; addr++) {
+        struct in_addr ip;
+        ip.s_addr = *(in_addr_t *)*addr;
+        printf("resolved ip is %s\n", inet_ntoa(ip));
+    }
+
+    return 0;
+}
