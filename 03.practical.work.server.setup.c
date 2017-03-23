2fddf16c8098dc44e4522df8f39de1e400e1cdc1
Dinh Ngoc Tu <none@none>
2017-03-23 18:27:34 +0700

    add practical work 3: server setup

---
+++ 03.practical.work.server.setup.c
@@ -0,0 +1,45 @@
+#define _GNU_SOURCE
+
+#include <arpa/inet.h>
+#include <error.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define SV_PORT 8784
+
+int main() {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("error creating sockfd");
+        return 1;
+    }
+
+    struct sockaddr_in sa;
+    memset(&sa, 0, sizeof(struct sockaddr_in));
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = htonl(INADDR_ANY);
+    sa.sin_port = htons(SV_PORT);
+
+    if (bind(sockfd, (struct sockaddr *)&sa, sizeof(struct sockaddr_in)) < 0) {
+        perror("error binding");
+        return 1;
+    }
+
+    if (listen(sockfd, 5) < 0) {
+        perror("error listening");
+        return 1;
+    }
+
+    while (1) {
+        struct sockaddr_in ca;
+        socklen_t clen = sizeof(struct sockaddr_in);
+        int clifd = accept(sockfd, (struct sockaddr *)&ca, &clen);
+        if (clifd < 0) {
+            perror("error accepting connection");
+        }
+
+        printf("client connected\n");
+    }
+}
